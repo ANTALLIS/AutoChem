5c33758bb7c586d9d07836d39361ab012a32bd47
Antonio <antonioallis@hotmail.co.uk>
2018-10-08 15:08:02 +0100

    Update Particle.java

--- Particle.java
+++ Particle.java
@@ -1,4 +1,6 @@
 public class Particle {
+	/* Class for a generic particle that behaves like a billiard ball */
+	
 	String name;
 	int id;
 	double mass;
@@ -8,6 +10,8 @@ public class Particle {
 	int[] colour;
 	
 	public Particle(String name, double mass, double radius, Vector3d position, Vector3d velocity, int[] colour) {
+		//Constructor method
+		
 		this.name = name;
 		this.mass = mass;
 		this.radius = radius;
@@ -17,14 +21,20 @@ public class Particle {
 	}
 	
 	public void update() {
+		// Update all attributes of the particle
+		
 		this.updatePosition();
 	}
 	
 	public void updatePosition() {
+		// Update the position of the particle
+		
 		position.add(velocity);
 	}
 	
 	public void updateVelocity(Vector3d acceleration) {
+		// Update the velocity of the particle
+		
 		velocity.add(acceleration);
 	}
 	
@@ -32,7 +42,9 @@ public class Particle {
 		a.position = Vector3d.add(a.postition, a.velocity);
 	}*/
 	
-	public boolean isCollision(Particle a, Particle b) {
+	public static boolean isCollision(Particle a, Particle b) {
+		// Check if two particles are colliding
+		
 		if(Vector3d.distance(a, b) < (a.radius + b.radius))
 			return true;
 		return false;
