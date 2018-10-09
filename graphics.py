2f44bdecd90dbf4443ea24573e1bb5fa96415165
JamanJaffry <36425669+JamanJaffry@users.noreply.github.com>
2018-10-09 11:36:21 +0100

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -4,5 +4,11 @@ import random
 
 pygame.init()
 
+display_width = 800
+display_height = 600 
+Main_dispay = pygame.display.set_mode((display_width,display_height))
+caption = pygame.display.set_caption('AutoChem')
+
+
 FPS = 30
-def user_interface
+def user_interface(FPS,)
