99cec5e10a23cb63517522fd0c64b6b8fd4ad67b
JamanJaffry <36425669+JamanJaffry@users.noreply.github.com>
2018-10-09 12:00:19 +0100

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -6,9 +6,32 @@ pygame.init()
 
 display_width = 800
 display_height = 600 
-Main_dispay = pygame.display.set_mode((display_width,display_height))
+Main_dispLay = pygame.display.set_mode((display_width,display_height))
 caption = pygame.display.set_caption('AutoChem')
 
+icon = pygame.image.load('icon.ico')
+
+pygame.display.set_icon(icon)
+
+background = pass 
+
+clock = pygame.time.Clock()
 
 FPS = 30
-def user_interface(FPS,)
+
+def user_interface(background):
+  exiti = False
+  while not exiti:
+    Main_dispLay.blit(background)
+    
+    pygame.display.update()
+    clock.tick(FPS)
+    
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              exiti = True
+                
+  pygame.quit()
+  quit()
+
+  
