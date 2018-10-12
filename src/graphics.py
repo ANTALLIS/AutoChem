c6188fc33e97498d3bc003e2b040ef06f198cce2
Antonio <antonioallis@hotmail.co.uk>
2018-10-12 20:54:45 +0100

    Moved some stuff around

---
+++ src/graphics.py
@@ -0,0 +1,43 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+display_width = 800
+display_height = 600 
+Main_dispLay = pygame.display.set_mode((display_width,display_height))
+caption = pygame.display.set_caption('AutoChem')
+
+icon = pygame.image.load('icon.ico')
+
+black = (0,0,0)
+
+
+
+pygame.display.set_icon(icon)
+
+background = pygame.image.load('BA.png')
+
+clock = pygame.time.Clock()
+
+FPS = 30
+
+def user_interface(background):
+  exiti = False
+  while not exiti:
+    Main_dispLay.blit(background)
+    
+    pygame.display.update()
+    clock.tick(FPS)
+    
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              exiti = True
+            
+   pygame.draw.rect(Main_dispLay,black,(450,500),(50,100))
+                
+  pygame.quit()
+  quit()
+
+  
