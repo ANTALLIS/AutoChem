9e776b5e9fe37d1513b7e5b0abeddce9a7d1658d
JamanJaffry <36425669+JamanJaffry@users.noreply.github.com>
2018-10-16 23:46:43 +0100

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -5,39 +5,63 @@ import random
 pygame.init()
 
 display_width = 800
-display_height = 600 
-Main_dispLay = pygame.display.set_mode((display_width,display_height))
-caption = pygame.display.set_caption('AutoChem')
+display_height = 600
+mainDisplay = pygame.display.set_mode((display_width,display_height))
 
-icon = pygame.image.load('icon.ico')
 
+#colour
+white = (255,255,255)
 black = (0,0,0)
+green = (69,219,46)
+red = (255,0,0)
+yellow = (255,255,0)
 
+pygame.display.set_caption('AutoChem')
 
+icn = pygame.image.load('icon.ico')
+pygame.display.set_icon(icn)
 
-pygame.display.set_icon(icon)
-
-background = pygame.image.load('BA.png')
-
+background = pygame.image.load('Background.png')
 clock = pygame.time.Clock()
 
-FPS = 30
+largefont = pygame.font.SysFont("couriernew", 80)
+medfont = pygame.font.SysFont("couriernew", 40)
 
-def user_interface(background):
-  exiti = False
-  while not exiti:
-    Main_dispLay.blit(background)
-    
-    pygame.display.update()
-    clock.tick(FPS)
-    
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-              exiti = True
-            
-   pygame.draw.rect(Main_dispLay,black,(450,500),(50,100))
-                
-  pygame.quit()
-  quit()
+#buttons
+start = pygame.image.load('start.png')
+opt = pygame.image.load('options.png')
+qui = pygame.image.load('quit.png')
 
-  
+
+FPS = 15
+
+def button(start,opt,qui):
+    #start
+    mainDisplay.blit(start,(340,500))
+    #quit
+    mainDisplay.blit(opt,(600,500))
+    #options
+
+    mainDisplay.blit(qui,(65,500))
+    
+    
+
+def menu(FPS,back):
+    game_Exit = False
+    while not game_Exit:
+        mainDisplay.blit(back,(0,0))
+        textsurface = largefont.render('AutoChem', False, white)
+        mainDisplay.blit(textsurface,(display_width/2-180,display_height/2-80))
+        button(start,opt,qui)
+        pygame.display.update()
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_Exit = True
+    pygame.quit()
+    quit()
+                    
+
+menu(FPS,background)
+ 
